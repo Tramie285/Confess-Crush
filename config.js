453a922602eb7cabc2e7cf3bb58bb00f98df4a4b
Tramie285 <100134293+Tramie285@users.noreply.github.com>
2022-02-21 19:33:29 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,17 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
-    btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
-    btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
+    titleWeb: "Gửi cho...<3",
+    introTitle: 'Lưu Khải',
+    introDesc: `Sau đây là những gì em muốn hỏi anh. Anh phải trả lời thật lòng, hông trả lời em oánh chết mẹ anh -_-`,
+    btnIntro: 'Zô!!',
+    title: 'Anh đã phạm phải tội tày đình!!',
+    desc: 'Đó là làm cho em yêu anh :< Giờ anh chịu trách nhiệm đi',
+    btnYes: 'Oke luôn nè',
+    btnNo: 'Không nha :))',
+    question: 'Anh có đồng ý nấu cơm cùng em cả đời này hông :> Trả lời "Có" hoặc "Không"',
+    btnReply: 'Gửi bé <3',
+    reply: 'Có',
+    mess: 'Em biết mà 🥰. Yêu anh nhìu nhìu 😘😘',
+    messDesc: 'Sau này em có bám dính lấy anh, anh cũng hông được chê em phiền đó nha :<',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messLink: 'me/tramiee2805' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
